e6102b0a38953550de271ab51075279f3e0e8db0
JanO Mnemonix <code@7an0.com>
2013-12-18 11:27:54 +0100

    DB benutzr final

--- benutzer.sql
+++ benutzer.sql
@@ -1,15 +1,17 @@
--- Datenbank backup
+-- Datenbank Backup
 
 CREATE TABLE IF NOT EXISTS `benutzer` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `passwort` varchar(34) NOT NULL,
   `bemerkung` varchar(120) DEFAULT NULL,
   `rolle` int(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1001 ;
 
 
-INSERT INTO `benutzer` (`id`, `email`, `password`, `bemerkung`, `rolle`) VALUES
-(1, 'admin@shop', 'asdf', NULL, 1),
-(2, 'kunde@shop', 'qwer', NULL, 10);
+INSERT INTO `benutzer` (`id`, `name`, `email`, `passwort`, `bemerkung`, `rolle`) VALUES
+(1, 'admin', 'admin@shop', 'asdf', NULL, 1),
+(2, 'Rene Descartes', 'kunde@shop', 'qwert', NULL, 10),
+(1000, 'Platon', 'kunde2@shop', 'qwert', NULL, 10);
