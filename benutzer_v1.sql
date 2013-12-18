f7a8557b50bbb47e742599fe6fb44a1f5fc74d3a
JanO Mnemonix <code@7an0.com>
2013-12-18 10:54:08 +0100

    DB

---
+++ benutzer_v1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `Benutzer` (
+	`id` INT( 5 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+	`email` VARCHAR( 30 ) NOT NULL ,
+	`password` VARCHAR( 32 ) NOT NULL ,
+	`bemerkung` VARCHAR( 120 ) NOT NULL ,
+	`rolle` INT( 2 ) NOT NULL
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_general_ci;
