b4d458d08e73c6071f53331c31f73b0e9b00d710
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-09 10:47:54 +0100

    Create bestellungen.sql

---
+++ SQL/bestellungen.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `bestellungen` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `kundeID` int(5) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `bemerkung` varchar(140) NOT NULL,
+  `status` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
