cefe9cfc1f793af559132cd3ecf0b254bca4194e
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-09 16:39:03 +0100

    Update tmp.sql

--- SQL/tmp.sql
+++ SQL/tmp.sql
@@ -1,1 +1,3 @@
 INSERT INTO `warenkorb` (`kundeID`, `wareID`, `anzahl`) VALUES ('1', '2', '1');
+
+UPDATE `dw54`.`warenkorb` SET `anzahl` = '7' WHERE `warenkorb`.`kundeID` =2 AND `warenkorb`.`wareID` =1;
