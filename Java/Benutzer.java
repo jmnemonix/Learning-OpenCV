e2643110e726c355769b566ab686d488c1da3eeb
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-10 07:15:28 +0100

    Benutzer noch nicht fertig

--- Java/Benutzer.java
+++ Java/Benutzer.java
@@ -20,7 +20,7 @@ import javax.servlet.http.*;
 
 public class Benutzer extends HttpServlet{
 
-	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
 		
 		String sqlUsr  = "dw54";
     	String sqlPswd = "";
@@ -64,8 +64,8 @@ public class Benutzer extends HttpServlet{
 						String zeile1 = "<form method='POST' action='http://praxi.mt.haw-hamburg.de/servlet/Benutzer'><p>Deine Kundennummer: "+uid+" und deine Rolle: "+urolle +"</p>";
 						String zeile2 = "<p>Dein Name: <input type='text' name='uname' value='"+uname+"'></p>";
 						String zeile3 = "<p>Deine E-Mail: <input type='email' name='uname' value='"+umail+"'></p>";
-						String zeile4 = "<p>Dein Passwort&auml;ndern: Aktuell: <input type='password' name='pswda'> Neu: <input type='password' name='pswdb'> Nochmal: <input type='password' name='pswdc'></p>";
-						String zeile5 = "<input id='update' type='submit' value='&Auml;ndern' name='absenden' disabled></form>";
+						String zeile4 = "<p>Dein Passwort&auml;ndern: Aktuell: <input type='password' name='pswda'> Neu: <input type='password' name='pswdb'> Nochmal: <input type='password' name='pswdc'><input type='checkbox' name='setPassword' value='myPassword'></p>";
+						String zeile5 = "<p><input type='hidden' name='f' value='change'></p><input id='update' type='submit' value='&Auml;ndern' name='absenden'></form>";
 
 						out.println( zeile1+zeile2+zeile3+zeile4+zeile5);
 
@@ -77,6 +77,32 @@ public class Benutzer extends HttpServlet{
 
 				}
 				else if (funktion.equals("change")){ // --------------------------------------------------------------------------------------------- Seine Daten aendern
+					String n_uname  = req.getParameter("uname");
+					String n_email  = req.getParameter("umail");
+					String n_pswd1  = req.getParameter("pswda");
+					String n_pswd2  = req.getParameter("pswdb");
+					String n_pswd3  = req.getParameter("pswdc");
+					String n_pswd4  = req.getParameter("setPassword");
+					// Erstmal alle variablen ausgeben
+
+					ResultSet rs = st.executeQuery("SELECT * FROM benutzer WHERE id LIKE '"+userID+"'");
+
+					boolean altesPswdRichtig = false;
+					boolean neueEmailSchonBekannt = false;
+
+					while(rs.next()){
+						String oname = rs.getString("name");
+						String oemail = rs.getString("email");
+						String opasswort = rs.getString("passwort");
+
+						if(opasswort.equals(n_pswd1)) altesPswdRichtig = true;
+					}
+					Statement st2 = con.createStatement();
+
+					ResultSet rs2 = st.executeQuery("SELECT * FROM benutzer WHERE email LIKE '"+n_email+"'");
+
+					//while(rs2.next()){ TODO: hier weiter machen
+
 
 				}
 				else if ((funktion.equals("delete"))&&(userRolle.equals("1"))) {
@@ -127,7 +153,7 @@ public class Benutzer extends HttpServlet{
 						String ukomme = rs.getString("bemerkung");
 						String upass  = rs.getString("passwort");
 
-						out.println( "<p>"+uid+" ("+urolle +") "+uname+" ("+umail+") <a href='index.jsp?p=admin&adm=5&uid="+uid+"'>Bearbeiten</a></p>");
+						out.println( "<p>"+uid+" ("+urolle +") "+uname+" ("+umail+") <a href='index.jsp?p=admin&adm=5&uid="+uid+"'>Bearbeiten</a><a href='/servlet/Benutzer?f=delete&uid="+uid+"'>L&ouml;schen</a></p>");
 
 					}
 
@@ -145,8 +171,8 @@ public class Benutzer extends HttpServlet{
 
 		
 	}
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
 		
-		doPost(req,res);
+		doGet(req,res);
 	}
 }
