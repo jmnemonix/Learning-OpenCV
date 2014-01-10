e1c0e58423f2b6d2c31ea673c1023c0c96001fd6
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-10 09:25:12 +0100

    Update tmp.sql

--- SQL/tmp.sql
+++ SQL/tmp.sql
@@ -3,5 +3,7 @@ INSERT INTO `warenkorb` (`kundeID`, `wareID`, `anzahl`) VALUES ('1', '2', '1');
 UPDATE `dw54`.`warenkorb` SET `anzahl` = '7' WHERE `warenkorb`.`kundeID` =2 AND `warenkorb`.`wareID` =1;
 UPDATE `dw54`.`bestellungen` SET `status` = '3' WHERE `bestellungen`.`id` =1;
 
-INSERT INTO `dw54`.`bestellungen` (`id` ,`kundeID` ,`adresse` ,`bemerkung` ,`status`)VALUES (NULL , '1000', 'rdbstr. 12 202020 Hamburg', 'bem', '1');
-INSERT INTO `dw54`.`bestWare` (`bestellungID` ,`position` ,`wareID` ,`anzahl`)VALUES ('3', '1', '11', '5');
+INSERT INTO `bestellungen` (`id` ,`kundeID` ,`adresse` ,`bemerkung` ,`status`)VALUES (NULL , '1000', 'rdbstr. 12 202020 Hamburg', 'bem', '1');
+INSERT INTO `bestWare` (`bestellungID` ,`position` ,`wareID` ,`anzahl`)VALUES ('3', '1', '11', '5');
+
+DELETE * FROM `warenkorb` WHERE `warenkorb`.`kundeID` = 1
