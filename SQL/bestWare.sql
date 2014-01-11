6a933c77e0f84d4c14c8f41ada9aeb7c4ffc81f7
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-11 14:22:35 +0100

    Update bestWare.sql
    FOREIGN KEYs --> bestellungen (inkl. delete cascade) & ware

--- SQL/bestWare.sql
+++ SQL/bestWare.sql
@@ -4,4 +4,12 @@ CREATE TABLE IF NOT EXISTS `bestWare` (
   `wareID` int(5) NOT NULL,
   `anzahl` int(6) NOT NULL,
   PRIMARY KEY (`bestellungID`,`position`)
+# der FOREIGN KEY dient als referenz auf die zugehörige Bestellung
+  FOREIGN KEY(bestellungID)
+    references bestellungen(id)
+# wird die Bestellung gelöscht, werden auch alle referenzierten artikel gelöscht
+    ON DELETE CASCADE
+# der FOREIGN KEY dient als referenz auf die bestellte Ware
+ FOREIGN KEY(wareID)
+    references ware(id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
