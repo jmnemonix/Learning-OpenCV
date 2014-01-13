ef2cc9cc6b2af24696891e5440c644950dba8f4d
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-13 09:01:35 +0100

    Update bestellungen.sql

--- SQL/bestellungen.sql
+++ SQL/bestellungen.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `bestellungen` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
-  `kundeID` int(5) NOT NULL,
+  `kundeID` int(5) NOT NULL REFERENCES penutzer(id),
   `adresse` varchar(50) NOT NULL,
   `bemerkung` varchar(140) NOT NULL,
   `status` int(2) NOT NULL,
