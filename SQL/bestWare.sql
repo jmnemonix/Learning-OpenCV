3eed74695d71c7c61a725e4ace061e25090fcb0c
JanO Mnemonix <jmnemonix@sl9.eu>
2014-01-13 09:08:39 +0100

    Update bestWare.sql

--- SQL/bestWare.sql
+++ SQL/bestWare.sql
@@ -6,10 +6,19 @@ CREATE TABLE IF NOT EXISTS `bestWare` (
   PRIMARY KEY (`bestellungID`,`position`),
 # der FOREIGN KEY dient als referenz auf die zugehörige Bestellung
   FOREIGN KEY(bestellungID)
-    REFERENCES bestellungen(id),
-# wird die Bestellung gelöscht, werden auch alle referenzierten artikel gelöscht
-    ON DELETE CASCADE,
+    REFERENCES bestellungen(id) ON DELETE CASCADE,
 # der FOREIGN KEY dient als referenz auf die bestellte Ware
  FOREIGN KEY(wareID)
-    REFERENCES ware(id)
+    REFERENCES ware(id) ON DELETE CASCADE
 );
+
+# bei naechstem einspielen in DB ggf. nicht mitnehmen
+
+INSERT INTO `bestWare` (`bestellungID`, `position`, `wareID`, `anzahl`) VALUES
+(1, 1, 2, 5),
+(1, 2, 5, 2),
+(2, 1, 3, 1),
+(3, 1, 11, 5),
+(4, 1, 6, 2),
+(4, 2, 7, 3),
+(4, 3, 8, 3);
