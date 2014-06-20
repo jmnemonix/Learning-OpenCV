9ab296f041d2d2862438c3bce9f85588a1c09a55
JanO Mnemonix <jmnemonix@sl9.eu>
2014-06-20 19:42:42 +0200

    tmp YT Source

---
+++ C++/quickstart/iGuy/opencvcmd.sh
@@ -0,0 +1,2 @@
+
+g++ -o $1 $1.cpp -I/usr/include/opencv-2.3.1 -I/usr/include/opencv-2.3.1/opencv -I/usr/include/opencv-2.3.1/opencv2 -L/usr/lib -lopencv_core -lopencv_imgproc -lopencv_calib3d -lopencv_video -lopencv_highgui -lopencv_ml -lopencv_features2d -lopencv_objdetect -lopencv_contrib -lopencv_legacy
