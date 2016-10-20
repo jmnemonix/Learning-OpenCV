48ecf11f1fea34c37f9392d43926643a751f46a6
JanO Mnemonix <jmnemonix@sl9.eu>
2016-10-20 18:37:16 +0200

    a

--- JavaClientServer/Java Client Server Programmierung/src/kapitel_2/u4_Socket/Sender.java
+++ JavaClientServer/Java Client Server Programmierung/src/kapitel_2/u4_Socket/Sender.java
@@ -1,42 +1,9 @@
 package kapitel_2.u4_Socket;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-
-import general.values.MyBuffer;
-
-public class Sender {
-	
-//	private static final int BUFSIZE = 508;
+public class Receiver {
 
 	public static void main(String[] args) {
-		
-		int port = Integer.parseInt( args[0] );
-		String remoteHost = args[1];
-		int remotePort = Integer.parseInt( args[2] );
-		
-		try ( DatagramSocket socket = new DatagramSocket(port) ) {
-			
-			socket.connect(InetAddress.getByName(remoteHost), remotePort);
-			
-			DatagramPacket packet = new  DatagramPacket(new byte[MyBuffer.SIZE], MyBuffer.SIZE);
-			
-			while (true) {
-				
-				socket.receive(packet);
-				String text = new String(packet.getData(), 0, packet.getLength() );
-				
-				System.out.println(packet.getAddress().getHostAddress() + ":" + packet.getPort() + " > " + text);
-				
-			}
-			
-		} catch ( IOException e ) {
-			System.err.println(e);
-		}
-		
-		
+		// TODO Auto-generated method stub
 
 	}
 
